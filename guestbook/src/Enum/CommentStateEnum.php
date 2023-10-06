eb7d4f68bc1efc883cc466576e9a25a2af9243c6
laurenttarallo tarallo <laurent.tarallo@orange.fr>
2023-10-06 13:39:59 +0000

    updateMessengerspam

--- guestbook/src/Enum/CommentStateEnum.php
+++ guestbook/src/Enum/CommentStateEnum.php
@@ -8,4 +8,8 @@ enum CommentStateEnum : string
 case SUBMITTED = 'submitted';
 case PUBLISHED = 'published';
 case SPAM = 'spam';
+case HAM = 'ham';
+case POTENTIAL_SPAM = 'potential_spam';
+case REJECTED = 'rejected';
+
 }
